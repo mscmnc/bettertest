5ebf669a8dac052db877dda89c3b5a17ae56b621
mscmnc <aurimas.zaleckas@gmail.com>
2018-04-22 12:59:56 +0300

    adding frontend files to github

---
+++ docs/js/new.js
@@ -0,0 +1,73 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+  account: 0x0,
+  loading: false,
+
+  init: function() {
+    return App.initWeb3();
+  },
+
+  initWeb3: function() {
+    // initialize web3
+    if(typeof web3 !== 'undefined') {
+      //reuse the provider of the Web3 object injected by Metamask
+      App.web3Provider = web3.currentProvider;
+    } else {
+      //create a new provider and plug it directly into our local node
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    web3 = new Web3(App.web3Provider);
+
+      App.updateTokenBalance();
+      App.displayAccountInfo();
+
+  return App.initContract();
+  },
+
+  initContract: function() {
+    $.getJSON('BetterToken.json', function(betterTokenArtifact) {
+      // get the contract artifact file and use it to instantiate a truffle contract abstraction
+      App.contracts.BetterToken = TruffleContract(betterTokenArtifact);
+      // set the provider for our contracts
+      App.contracts.BetterToken.setProvider(App.web3Provider);
+
+      return;
+
+    });
+  },
+
+  updateTokenBalance: function() {
+      var betterTokenInstance;
+      app.contractsBetterToken.deployed().then(function(instance) {
+      betterTokenInstance = instance;
+      return betterTokenInstance.balanceOf("0x627306090abaB3A6e1400e9345bC60c78a8BEf57");
+    }).then(function(value) {
+      console.log(value);
+      var balance_element = document.getElementById("balance");
+      balance_element.innerHTML = value.valueOf();
+    }).catch(function(e) {
+      console.log(e);
+    });
+  },
+
+  displayAccountInfo: function() {
+    web3.eth.getCoinbase(function(err, account) {
+      if(err === null) {
+        App.account = account;
+        $('#account').text(account);
+        web3.eth.getBalance(account, function(err, balance) {
+          if(err === null) {
+            $('#accountBalance').text(web3.fromWei(balance, "ether") + " ETH");
+          }
+        })
+      }
+    });
+  },
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  }
+});
+};
